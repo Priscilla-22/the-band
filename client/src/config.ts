1cdd1744761f9b2ba4516f7c18b379b796215cde
priscilla-22 <2p2@wakahia>
2025-02-09 18:57:38 +0300

    refactor: base url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://the-band-w4bx.onrender.com',
+    BASE_URL: 'https://the-band-w4bx.onrender.com',
 };
 
 export default config;
